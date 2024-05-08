18133bef92c6ac097947d83190f89191ee78b3bc
davidfahmi <david.mailbox86@gmail.com>
2024-05-08 21:37:15 +0700

    next commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+
+var slideindex = 1;
+showDivs (slideindex);
+
+function plusDivs(n) {
+    showDivs((slideindex += n ));
+}
+
+function showDivs(n) {
+    var i;
+    var imglist = document.getElementsByClassName("img-slideshow");
+    if (n > imglist.length) slideindex =1;
+    else if (n<1)slideindex =imglist.length;
+
+    for ( i=0 ;i < imglist.length; i++) {
+        imglist[i].style.display = "none";
+    }
+    imglist[slideindex - 1].style.display = "block";
+    console.log(slideindex);
+}
+
+
+
+
 function validateform  () {
     const forminputname = document.forms['forminput']['fullname_input'].value
     const forminputemail = document.forms['forminput']['email_input'].value
